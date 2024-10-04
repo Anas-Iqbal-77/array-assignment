73531a9879d9db7628dd5da49ed628c6d48d718d
Anas-Iqbal-77 <iqbalanas840@gmail.com>
2024-10-04 23:45:58 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,153 @@
+
+//let studentNamesLiteral = [];
+
+
+//let studentNamesObject = new Array();
+
+//let stringArray = ['Apple', 'Banana', 'Cherry'];
+
+
+//let numbersArray = [1, 2, 3, 4, 5];
+
+
+//let booleanArray = [true, false, true, false];
+
+
+//let mixedArray = ['Hello', 42, true, 'World'];
+
+
+//let qualifications = ['SSC', 'HSC', 'BCS', 'BS', 'BCOM', 'MS', 'M. Phil.', 'PhD'];
+
+
+//document.write('Qualifications in Pakistan: ' + qualifications.join(', '));
+
+
+//const student = {
+    //name: "John",
+    //score: 320,
+    //totalMarks: 500
+  //};
+  
+  //const percentage = (student.score / student.totalMarks) * 100;
+  
+  //document.write(`Score of ${student.name} is ${student.score}. Percentage: ${percentage}%`);
+
+
+  //    document.getElementById("colorArray").innerHTML = "Original Array: " + colors.join(", ");
+
+    //   let colorToAddAtBeginning = prompt("Enter a color to add to the beginning:");
+    //colors.unshift(colorToAddAtBeginning);
+    //document.getElementById("colorArray").innerHTML += "<br>After adding color to the beginning: " + colors.join(", ");
+
+    
+    //let colorToAddAtEnd = prompt("Enter a color to add to the end:");
+    //colors.push(colorToAddAtEnd);
+    //document.getElementById("colorArray").innerHTML += "<br>After adding color to the end: " + colors.join(", ");
+
+    
+    //colors.unshift("Orange", "Purple");
+    //document.getElementById("colorArray").innerHTML += "<br>After adding two more colors to the beginning: " + colors.join(", ");
+
+    
+    //colors.shift();
+    //document.getElementById("colorArray").innerHTML += "<br>After deleting the first color: " + colors.join(", ");
+
+    
+    //colors.pop();
+    //document.getElementById("colorArray").innerHTML += "<br>After deleting the last color: " + colors.join(", ");
+
+    
+    //let indexToAddColor = parseInt(prompt("At which index do you want to add a color? (0 to " + (colors.length - 1) + "):"));
+    //let colorToAddAtIndex = prompt("Enter the color to add at index " + indexToAddColor + ":");
+    //colors.splice(indexToAddColor, 0, colorToAddAtIndex);
+    //document.getElementById("colorArray").innerHTML += "<br>After adding color at index " + indexToAddColor + ": " + colors.join(", ");
+
+    
+    //let indexToDeleteColor = parseInt(prompt("At which index do you want to delete color(s)? (0 to " + (colors.length - 1) + "):"));
+    //let howManyToDelete = parseInt(prompt("How many colors do you want to delete?"));
+    //colors.splice(indexToDeleteColor, howManyToDelete);
+    //document.getElementById("colorArray").innerHTML += "<br>After deleting " + howManyToDelete + " color(s) from index " + indexToDeleteColor + ": " + colors.join(", ");
+  
+  
+
+    //const scores = [320, 230, 480, 120];
+
+
+    //scores.sort((a, b) => a - b);
+
+
+    //console.log("Ordered Scores of Students:", scores.join(",")); 
+
+
+
+    //const cities = ["Karachi", "Lahore", "Islamabad", "Quetta", "Peshawar"];
+//const selectedCities = cities.slice(2, 5); // Copy 3 elements from index 2
+
+//document.write("Cities list:");
+//document.write(cities);
+
+//document.write("\nSelected cities list:");
+//document.write(selectedCities);
+
+
+//var arr = ["This ", "is ", "my ", "cat"];
+//var str = arr.join("");  // Join the array elements without any separator
+//console.log("Array:", arr);
+//console.log("String:", str); // Output: This is my cat
+
+
+
+
+//var arr2 = ["apple", "banana", "cherry"];
+//var str2 = arr2.join(", "); 
+//document.write("Array:", arr2);
+//document.write("String:", str2); 
+
+
+
+const devices = [];
+
+
+//devices.push("keyboard");
+//devices.push("mouse");
+//devices.push("printer");
+//devices.push("monitor");
+
+
+//document.write("First:", devices.shift()); 
+//document.write("Next:", devices.shift());  
+//document.write("Then:", devices.shift());  
+//document.write("Last:", devices.shift());  
+
+
+
+//const deviceTypes = [];
+
+
+//deviceTypes.push("monitor");
+//deviceTypes.push("printer");
+//deviceTypes.push("mouse");
+//deviceTypes.push("keyboard");
+
+
+//console.log("Devices in reverse order:");
+//for (let i = deviceTypes.length - 1; i >= 0; i--) {
+// document.write(deviceTypes[i]);
+//}
+
+
+
+//deviceTypes.reverse();
+//console.log("\nDevices in reverse order using reverse():")
+//for (let i = 0; i < deviceTypes.length; i++){
+  //  document.write(deviceTypes[i])
+//}
+
+
+//const manufacturers = ["Apple", "Samsung", "Motorola", "Nokia", "Sony", "Haier"];
+
+  //document.write("<select>");
+  //for (let i = 0; i < manufacturers.length; i++) {
+    //document.write(`<option value="${manufacturers[i]}">${manufacturers[i]}</option>`);
+  //}
+  //document.write("</select>");
